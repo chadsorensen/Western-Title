a3a77c04a9ec68501390799da6bb5182731154bc
chadsorensen <chdsorensen@gmail.com>
2014-01-30 22:44:52 -0800

    initial setup

---
+++ header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/modernizr-2.6.2.min.js"></script>
+  </head>
+  <body>
+    <!--[if lt IE 7]>
+        <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+
+    <!-- Add your site or application content here -->
+  <section id="header">
+    <h1 class="logo ir">Western Title &amp; Escrow</h1>
+    <h2 class="tagline">A partnership <em>beyond expectations</em>.</h2>
+    <nav>
+      <a href="#" title="">account login</a>
+      /
+      <a href="#" title="">register</a>
+    </nav>
+    <div class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+                <li class="divider"></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!-- /.nav-collapse -->
+      </div><!-- /.container -->
+    </div>
+  </section>
---
+++ index.php
@@ -0,0 +1,142 @@
+<?php include 'header.php' ?>
+
+  <div id="heroCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1" class=""></li>
+      <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+      <li data-target="#myCarousel" data-slide-to="3" class=""></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="item active">
+        <img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="img/slider-1.jpg">
+        <div class="container">
+          <div class="carousel-caption">
+            <p>photo: credit here</p>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" alt="Second slide" src="img/slider-2.jpg">
+        <div class="container">
+          <div class="carousel-caption">
+            <p>photo: credit here</p>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img data-src="holder.js/900x500/auto/#555:#5a5a5a/text:Third slide" alt="Third slide" src="img/slider-3.jpg">
+        <div class="container">
+          <div class="carousel-caption">
+            <p>photo: credit here</p>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img data-src="holder.js/900x500/auto/#555:#5a5a5a/text:Fourth slide" alt="Fourth slide" src="img/slider-4.jpg">
+        <div class="container">
+          <div class="carousel-caption">
+            <p>photo: credit here</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+<section id="tools" class="content-container">
+
+  <header class="">
+    <h1>Tool Dashboard</h1>
+    <nav>
+      <li>Residential Real Estate</li>
+      <li>Commercial Real Estate</li>
+      <li>Lenders</li>
+    </nav>
+  </header>
+
+  <div class="row">
+    <div class="column">
+      <span class="icon icon-list"></span>
+      <h2>Quick List Kit</h2>
+      <p>Lorem ipsum dolor set consecteteur lorem ipsum dolor set amet.</p>
+    </div>
+
+    <div class="column">
+      <span class="icon icon-list"></span>
+      <h2>Quick List Kit</h2>
+      <p>Lorem ipsum dolor set consecteteur lorem ipsum dolor set amet.</p>
+    </div>
+
+    <div class="column">
+      <span class="icon icon-list"></span>
+      <h2>Quick List Kit</h2>
+      <p>Lorem ipsum dolor set consecteteur lorem ipsum dolor set amet.</p>
+    </div>
+
+    <div class="column last-child">
+      <span class="icon icon-list"></span>
+      <h2>Quick List Kit</h2>
+      <p>Lorem ipsum dolor set consecteteur lorem ipsum dolor set amet.</p>
+    </div>
+
+    <div class="column">
+      <span class="icon icon-list"></span>
+      <h2>Quick List Kit</h2>
+      <p>Lorem ipsum dolor set consecteteur lorem ipsum dolor set amet.</p>
+    </div>
+
+    <div class="column">
+      <span class="icon icon-list"></span>
+      <h2>Quick List Kit</h2>
+      <p>Lorem ipsum dolor set consecteteur lorem ipsum dolor set amet.</p>
+    </div>
+
+    <div class="column">
+      <span class="icon icon-list"></span>
+      <h2>Quick List Kit</h2>
+      <p>Lorem ipsum dolor set consecteteur lorem ipsum dolor set amet.</p>
+    </div>
+
+    <div class="column last-child">
+      <span class="icon icon-list"></span>
+      <h2>Quick List Kit</h2>
+      <p>Lorem ipsum dolor set consecteteur lorem ipsum dolor set amet.</p>
+    </div>
+
+  </div>
+
+</section>
+
+<section id="trends">
+  <div class="content-container">
+
+    <header>
+      <h1>Trends</h1>
+      <a href="#" title="">view all trends</a>
+    </header>
+
+    <div class="trends-container">
+      <img src="img/trends-1.jpg" alt="">
+      <div class="content">
+        <h2>Infographic Title</h2>
+        <p class="date">Jan. 1, 2014</p>
+        <p class="summary">Subhead or description goes here lorem ispum dolor sit amet.</p>
+      </div>
+    </div>
+
+    <div class="trends-container">
+      <img src="img/trends-2.jpg" alt="">
+      <div class="content">
+        <h2>Infographic Title</h2>
+        <p class="date">Jan. 1, 2014</p>
+        <p class="summary">Subhead or description goes here lorem ispum dolor sit amet.</p>
+      </div>
+    </div>
+
+  </div>
+
+</section>
+
+<?php include 'footer.php' ?>
