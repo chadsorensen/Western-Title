e28398aba8a21f4d8c439763eee9049ed429e9e8
chadsorensen <chdsorensen@gmail.com>
2014-02-04 23:06:04 -0800

    added team directory template and basic styles

---
+++ team.php
@@ -0,0 +1,58 @@
+<?php include 'header.php' ?>
+<div class="header-bar">
+  <h1>Team Directory</h1>
+</div>
+<div class="main-wrapper">
+  <nav class="breadcrumbs">
+    <ul>
+      <li><a href="/" title="">home</a></li> &raquo;
+      <li class="active">team directory</li>
+    </ul>
+  </nav>
+  <div class="filters">
+
+  </div>
+  <div class="team-listing main-content">
+    <div class="team-item col-2_5-4-12">
+      <img src="images/team-directory.jpg" alt="">
+      <h3 class="name">Robert A. Sampleman</h3>
+      <h4 class="title">VP of Professional Title</h4>
+      <p class="location">Location: Bend, OR</p>
+      <p class="tel">Tel: 541.123.4567</p>
+      <a href="mailto:email@westerntitle.com">email@westerntitle.com</a>
+    </div>
+    <div class="team-item col-2_5-4-12">
+      <img src="images/team-directory.jpg" alt="">
+      <h3 class="name">Robert A. Sampleman</h3>
+      <h4 class="title">VP of Professional Title</h4>
+      <p class="location">Location: Bend, OR</p>
+      <p class="tel">Tel: 541.123.4567</p>
+      <a href="mailto:email@westerntitle.com">email@westerntitle.com</a>
+    </div>
+    <div class="team-item col-2_5-4-12">
+      <img src="images/team-directory.jpg" alt="">
+      <h3 class="name">Robert A. Sampleman</h3>
+      <h4 class="title">VP of Professional Title</h4>
+      <p class="location">Location: Bend, OR</p>
+      <p class="tel">Tel: 541.123.4567</p>
+      <a href="mailto:email@westerntitle.com">email@westerntitle.com</a>
+    </div>
+    <div class="team-item col-2_5-4-12">
+      <img src="images/team-directory.jpg" alt="">
+      <h3 class="name">Robert A. Sampleman</h3>
+      <h4 class="title">VP of Professional Title</h4>
+      <p class="location">Location: Bend, OR</p>
+      <p class="tel">Tel: 541.123.4567</p>
+      <a href="mailto:email@westerntitle.com">email@westerntitle.com</a>
+    </div>
+    <div class="team-item col-2_5-4-12">
+      <img src="images/team-directory.jpg" alt="">
+      <h3 class="name">Robert A. Sampleman</h3>
+      <h4 class="title">VP of Professional Title</h4>
+      <p class="location">Location: Bend, OR</p>
+      <p class="tel">Tel: 541.123.4567</p>
+      <a href="mailto:email@westerntitle.com">email@westerntitle.com</a>
+    </div>
+  </div>
+</div>
+<?php include 'footer.php' ?>
